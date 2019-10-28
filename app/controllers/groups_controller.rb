57d952fc740a128248692df90dbccf993442feb1
herohero1022 <eq646261@gmail.com>
2019-10-28 17:29:36 +0900

    content_sideの部分を共通テンプレート化

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,30 @@
 class GroupsController < ApplicationController
- def new
-  @group = Group.new
-  @group.users << current_user
- end
 
- def create
-  @group = Group.new(group_params)
-  if @group.save
-    redirect_to root_path, notice: 'グループを作成しました'
-  else
-    render :new
+  def index
   end
- end
 
- def edit
- end
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
 
- def update
- end
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
 
- private
- def group_params
-   params.require(:group).permit(:name, user_ids: [] )
- end
+  def edit
+  end
 
+  def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
 end
