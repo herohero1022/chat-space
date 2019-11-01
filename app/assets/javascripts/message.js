bc96e6aad08c13aec7aec2a7483dbf274ebcff09
herohero1022 <eq646261@gmail.com>
2019-11-01 10:26:23 +0900

    非同期通信実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,32 @@
 $(function(){
+
+  function buildhtml(message){
+    var addimage = message.image.url ? `<img class="message-text__image" src=${message.image.url}>` : "";
+    var html = `<div class="message">
+                <div class="message__info">
+                  <p class="message__info--talker">
+                    ${message.user_name}
+                  </p>
+                  <p class="message__info--date">
+                    ${message.created_at}
+                  </p>
+                </div>
+                <div class="message__text">
+                  <p class="message__text">
+                    ${message.content}
+                  </p>
+                </div>
+                <div class="lower-message__image">
+                  ${addimage}
+                </div>
+               </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: 'POST',
@@ -11,5 +35,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(message){
+      var html = buildhtml(message);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},'fast' );
+      $('.form__submit').attr('disabled', false);
+      $('.form__message').val('')
+    })
+    .fail(function(message){
+      alert('メッセージ送信に失敗しました');
+    })
   })
-})
+});
